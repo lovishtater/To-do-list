495f0c8561e74ff3477578257111929c21b6795f
lovish tater <lovishtater@gmail.com>
2020-10-04 17:00:04 +0530

    added css

--- app.js
+++ app.js
@@ -4,18 +4,23 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
-app.set('view engine', 'ejs');  //app.use
+app.set('view engine', 'ejs'); //app.use
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(express.static("public"));
+
 let items = ['drink water', 'eat food'];
 
-
 app.get("/", function (req, res) {
-        res.render('lists',   {
-            htmlItems : items });
+    res.render('lists', {
+        htmlItems: items
+    });
 });
 
-app.post("/", function(req,res){
+app.post("/", function (req, res) {
     let input = req.body.input;
     items.push(input);
     console.log(items);
