ec7184683e4f8ceac2d1cf28c1a87503f8d3412c
lovish tater <lovishtater@gmail.com>
2020-10-04 15:57:56 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.set('view engine', 'ejs');  //app.use
+
+app.use(bodyParser.urlencoded({ extended: false }));
+let items = ['drink water', 'eat food'];
+
+
+app.get("/", function (req, res) {
+        res.render('lists',   {
+            htmlItems : items });
+});
+
+app.post("/", function(req,res){
+    let input = req.body.input;
+    items.push(input);
+    console.log(items);
+    res.redirect("/");
+});
+
+
+
+app.listen(3000, function () {
+    console.log("Server started on port 3000.");
+});
